4361742ff6abb14ec0e5842dc4f72c151a65826b
shajn786 <shajn786@gmail.com>
2022-12-09 14:20:56 +0530

    page design and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
+import { UsernavbarComponent } from './usernavbar/usernavbar.component';
+
+const myRoutes : Routes=[
+
+  {
+    path:"",component:AdminLoginComponent
+  },
+  {
+    path:"userlogin",component:UserLoginComponent
+  },
+  {
+    path:"register",component:RegistrationComponent
+  },
+  {
+    path:"dashboard",component:AdminDashboardComponent
+  },
+  {
+    path:"viewprofile",component:ViewProfileComponent
+  }
+
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    UserLoginComponent,
+    RegistrationComponent,
+    AdminDashboardComponent,
+    ViewProfileComponent,
+    UsernavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+
+  name=""
+  address=""
+  phno=""
+  email=""
+  password=""
+  confirmpassword=""
+
+  readValues=()=>
+  {
+    let data:any ={"name":this.name,"address":this.address,"phno":this.phno,"email":this.password,"confirmpassword":this.confirmpassword}
+    console.log(data)
+  }
+
+
+}
